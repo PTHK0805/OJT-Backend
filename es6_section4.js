a61891d95a8a357fa6ba9758f311850f6234e633
PTHK0805 <ptkyaw505@gmail.com>
2022-09-26 14:33:45 +0630

    Getters and Setters.

--- es6_section4.js
+++ es6_section4.js
@@ -1,12 +1,39 @@
-class Person{
+class Person {
   constructor(name) {
     this.name = name;
   }
 
-  getName() {
-    console.log(this.name);
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    newName = newName.trim();
+    if (newName === '') {
+      throw new Error('The name cannot be empty').message;
+    }
+    this._name = newName;
+ 
   }
 }
+const person = new Person('John Smith');
+console.log(person.name);
 
-const person = new Person('John');
-person.getName();
+
+let meeting = {
+  attendees: [],
+  add(attendee) {
+      console.log(`${attendee} joined the meeting.`);
+      this.attendees.push(attendee);
+      return this;
+  },
+  get latest() {
+      let count = this.attendees.length;
+      return count == 0 ? undefined : this.attendees[count - 1];
+  }
+};
+
+meeting.add('John').add('Jane').add('Peter');
+console.log(`The latest attendee is ${meeting.latest}.`);
+console.log(meeting.attendees);
+
