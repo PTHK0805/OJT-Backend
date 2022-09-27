fc9f204924b77c8b6c5f4493b1ce57e4d6b9e18f
PTHK0805 <ptkyaw505@gmail.com>
2022-09-27 14:46:14 +0630

    Array findIndex().

--- es6_section10.js
+++ es6_section10.js
@@ -82,3 +82,26 @@ for (let i = 0; i < customers.length; i++) {
 }
 console.log(newUser);
 
+// Array Find Index
+//let ranks = [1, 5, 7, 8, 10, 7];
+//let index = ranks.findIndex(rank => rank === 7);
+//console.log(index);
+
+let ranks = [1, 5, 7, 8, 10, 7];
+
+let index = ranks.findIndex(
+    (rank, index) => rank === 7 && index > 2
+);
+
+console.log(index);
+
+const products = [
+  { name: 'Phone', price: 999 },
+  { name: 'Computer', price: 1999 },
+  { name: 'Tablet', price: 995 },
+];
+
+const _index = products.findIndex(product => product.price > 1000);
+
+console.log(_index); // 1
+
