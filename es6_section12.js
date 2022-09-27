2b1eabfef2aa3573320113a91c6f8e98c48a31a6
PTHK0805 <ptkyaw505@gmail.com>
2022-09-27 15:21:08 +0630

    String startsWith().

---
+++ es6_section12.js
@@ -0,0 +1,10 @@
+
+let title = 'Jack and Jill Went Up the Hill';
+
+// Start With
+console.log(title.startsWith('Jack'));
+console.log(title.startsWith('jack'));
+console.log(title.startsWith('Jill', 10));
+
+console.log();
+
