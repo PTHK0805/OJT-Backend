8bdaa33493d880c0bf563c9508bcabe08c1cdbda
PTHK0805 <ptkyaw505@gmail.com>
2022-09-27 13:05:12 +0630

    Error Handling with (Async and Await).

--- es6_section8_error_handling.js
+++ es6_section8_error_handling.js
@@ -1,7 +1,9 @@
+const authorized = true;
+
 function getUserById(id) {
   return new Promise((resolve, reject) => {
-    if (!authorized) {
-      reject('Unauthorized access to the user data');
+    if (typeof id !== 'number') {
+      reject('ID Must be a valid number');
     }
     resolve({
       id: id,
@@ -10,12 +12,27 @@ function getUserById(id) {
   });
 }
 
-try {
-  getUserById(10)
-    .then(user => console.log(user.username))
-    //.catch(err => console.error(err));
-  console.log('next');
+//try {
+//  getUserById(10)
+//    .then(user => console.log(user.username))
+//    //.catch(err => console.error(err));
+//  console.log('next');
+//
+//} catch (error) {
+//  console.log(`Caught by try/catch ${error}`);
+//}
 
-} catch (error) {
-  console.log(`Caught by try/catch ${error}`);
+
+
+async function getUser() {
+  try {
+    const user = await getUserById('1');
+    console.log(user);
+  }
+  catch (error) { 
+    console.log(error);
+  }
 }
+
+getUser();
+
