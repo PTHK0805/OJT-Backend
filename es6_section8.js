522606a41ee012c289092c7f92eafdfce2cd4583
PTHK0805 <ptkyaw505@gmail.com>
2022-09-27 08:58:58 +0630

    Promise.

--- es6_section8.js
+++ es6_section8.js
@@ -1,24 +1,58 @@
-function getUsers(callback) {
+//function getUsers(callback) {
+//  console.log('Getting Users from Database...')
+//  setTimeout(() => {
+//    callback([
+//      { username: 'john', email: 'john@test.com' },
+//      { username: 'jane', email: 'jane@test.com' },
+//    ]);
+//  }, 4000);
+//
+//}
+//
+//function findUser(username, callback) {
+//  getUsers(users => {
+//    console.log(users);
+//    console.log('Finding User With Given Username...')
+//    setTimeout(() => {
+//      const user = users.find(user => user.username === username);
+//      callback(user);
+//    }, 4000)
+//
+//  })
+//}
+//
+//findUser('john', user => console.log(user));
+
+// Promise
+function getUsers() {
   console.log('Getting Users from Database...')
-  setTimeout(() => {
-    callback([
-      { username: 'john', email: 'john@test.com' },
-      { username: 'jane', email: 'jane@test.com' },
-    ]);
-  }, 4000);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        { username: 'john', email: 'john@test.com' },
+        { username: 'jane', email: 'jane@test.com' },
+      ]);
+    }, 4000);
+  })
 
 }
 
 function findUser(username, callback) {
-  getUsers(users => {
-    console.log(users);
-    console.log('Finding User With Given Username...')
-    setTimeout(() => {
-      const user = users.find(user => user.username === username);
-      callback(user);
-    }, 4000)
+  return new Promise((resolve, reject) => {
+    getUsers().then(users => {
+      console.log(users);
+      console.log('Finding User With Given Username...')
+      setTimeout(() => {
+        const user = users.find(user => user.username === username);
 
+        if (user) resolve(user);
+
+        reject(new Error("Given Username can't find...").message);
+      }, 4000)
+
+    })
   })
+
 }
 
-findUser('john', user => console.log(user));
+findUser('John').then(result => console.log(result)).catch(err => console.log(err));
