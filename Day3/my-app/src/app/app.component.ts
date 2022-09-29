3e49e00dba556dc0d17f2a035192881eb3063cb2
PTHK0805 <ptkyaw505@gmail.com>
2022-09-29 10:44:41 +0630

    Day4.

--- Day3/my-app/src/app/app.component.ts
+++ Day3/my-app/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DebugService } from './services/debug.service';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  title = 'my-app';
+@Component({ 
+  selector: 'app-root', 
+  templateUrl: `<h1>{{ title }}</h1>`, 
+  styleUrls: ['./app.component.css'] 
+}) 
+export class AppComponent implements OnInit { 
+  title = 'Expense Manager'; 
+  constructor(private debugService : DebugService) {} ngOnInit() { 
+     this.debugService.info("Angular Application starts"); 
+  } 
 }
