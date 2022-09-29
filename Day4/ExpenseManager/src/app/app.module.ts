2462d67f14221bf374b12179753406a88b960c5a
PTHK0805 <ptkyaw505@gmail.com>
2022-09-29 13:54:05 +0630

    Event Binding.

--- Day4/ExpenseManager/src/app/app.module.ts
+++ Day4/ExpenseManager/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExpenseEntryComponent
+    ExpenseEntryComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
