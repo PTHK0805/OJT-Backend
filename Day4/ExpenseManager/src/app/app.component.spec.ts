f2e3974129c34c55b15338903184200468ef2d1d
PTHK0805 <ptkyaw505@gmail.com>
2022-09-29 11:30:38 +0630

    init

--- Day4/ExpenseManager/src/app/app.component.spec.ts
+++ Day4/ExpenseManager/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'expense-manager'`, () => {
+  it(`should have as title 'ExpenseManager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('expense-manager');
+    expect(app.title).toEqual('ExpenseManager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('expense-manager app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ExpenseManager app is running!');
   });
 });
--- Day4/ExpenseManager/src/app/expense-entry/expense-entry.component.ts
+++ Day4/ExpenseManager/src/app/expense-entry/expense-entry.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-expense-entry',
   templateUrl: './expense-entry.component.html',
-  styleUrls: ['./expense-entry.component.scss']
+  styleUrls: ['./expense-entry.component.scss'],
+  
 })
 export class ExpenseEntryComponent implements OnInit {
-
+  title: string = '';
   constructor() { }
 
   ngOnInit(): void {
+    this.title = 'Expense Entry';
   }
 
 }
