a7c02e641cd7b2b7edd40be99ff3dee8f754ce1e
PTHK0805 <ptkyaw505@gmail.com>
2022-09-29 12:56:42 +0630

    Bootstrap

--- Day4/ExpenseManager/src/app/app.module.ts
+++ Day4/ExpenseManager/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { DebugService } from './services/debug.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -14,7 +15,9 @@ import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    DebugService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
