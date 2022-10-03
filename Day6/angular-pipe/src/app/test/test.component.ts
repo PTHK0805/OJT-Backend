a2fcd38a0bc68517a7c0e609affcba8ecc65f356
PTHK0805 <ptkyaw505@gmail.com>
2022-10-03 11:50:04 +0630

    Decimal Pipe.

--- Day6/angular-pipe/src/app/test/test.component.ts
+++ Day6/angular-pipe/src/app/test/test.component.ts
@@ -2,38 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, Observer, Subject } from 'rxjs';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+    selector: 'app-test',
+    templateUrl: './test.component.html',
+    styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
-  price: number = 20000;
-  Fruits = ["Apple","Orange","Grapes","Mango","Kiwi","Pomegranate"];
-  presentDate = new Date(); 
-  //timeChange: any;
-  
-  //timeChange = new Observable((observer: Observer<string>) => {
-  //  setInterval(() => observer.next(new
-  //  Date().toString()), 1000);
-  //});
+    price: number = 20000;
+    Fruits = ["Apple", "Orange", "Grapes", "Mango", "Kiwi", "Pomegranate"];
+    decimalNum1: number = 8.7589623;
+    decimalNum2: number = 5.43;
+    presentDate = new Date();
+    //timeChange: any;
 
-  //timeChange = new Observable((observer) => {
-  //  setInterval(() => {
-  //    observer.next(new Date().toString())
-  //  },1000)
-  //})
+    //timeChange = new Observable((observer: Observer<string>) => {
+    //  setInterval(() => observer.next(new
+    //  Date().toString()), 1000);
+    //});
 
-  
-  //ngOnInit() { 
-  //setInterval(() => {
-  //  this.timeChange = new Date();
-  //},1000)
-  //}
-  //
-  timeChange = new Subject();
-  ngOnInit() {
-    setInterval(() => {
-      this.timeChange.next(new Date().toString());
-    },1000)
-  }
+    //timeChange = new Observable((observer) => {
+    //  setInterval(() => {
+    //    observer.next(new Date().toString())
+    //  },1000)
+    //})
+
+
+    //ngOnInit() { 
+    //setInterval(() => {
+    //  this.timeChange = new Date();
+    //},1000)
+    //}
+    //
+    timeChange = new Subject();
+    ngOnInit() {
+        setInterval(() => {
+            this.timeChange.next(new Date().toString());
+        }, 1000)
+    }
 }
