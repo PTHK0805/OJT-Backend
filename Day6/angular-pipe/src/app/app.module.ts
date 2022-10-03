d60fa4f262b0773d4c9b200eb7d238a36de63e45
PTHK0805 <ptkyaw505@gmail.com>
2022-10-03 13:26:07 +0630

    Custom Pipe.

--- Day6/angular-pipe/src/app/app.module.ts
+++ Day6/angular-pipe/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { DigitcountPipe } from './pipes/digitcount.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
+    DigitcountPipe,
   ],
   imports: [
     BrowserModule,
--- Day6/angular-pipe/src/app/test/test.component.ts
+++ Day6/angular-pipe/src/app/test/test.component.ts
@@ -12,7 +12,9 @@ export class TestComponent implements OnInit {
     decimalNum1: number = 8.7589623;
     decimalNum2: number = 5.43;
     decimalNum3: number = 0.8178; 
-    jsonData = { id: 'one', name: { username: 'user1' }} 
+    jsonData = { id: 'one', name: { username: 'user1' } };
+    digits: number = 10000000000; 
+    
     presentDate = new Date();
     //timeChange: any;
 
@@ -28,16 +30,64 @@ export class TestComponent implements OnInit {
     //})
 
 
-    //ngOnInit() { 
+    //ngOnInit() {
     //setInterval(() => {
     //  this.timeChange = new Date();
     //},1000)
     //}
     //
+    
+    getExpenseEntries(){ 
+        let mockExpenseEntries = [ 
+           { id: 1, 
+              item: "Pizza", 
+              amount: Math.floor((Math.random() * 10) + 1), 
+              category: "Food", 
+              location: "Mcdonald", 
+              spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+              createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) }, 
+           { id: 1, 
+              item: "Pizza", 
+              amount: Math.floor((Math.random() * 10) + 1), 
+              category: "Food", 
+              location: "KFC", 
+              spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+              createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) }, 
+           { id: 1,
+              item: "Pizza",
+              amount: Math.floor((Math.random() * 10) + 1), 
+              category: "Food", 
+              location: "Mcdonald", 
+              spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+              createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) }, 
+           { id: 1, 
+              item: "Pizza", 
+              amount: Math.floor((Math.random() * 10) + 1), 
+              category: "Food", 
+              location: "KFC", 
+              spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+              createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) }, 
+           { id: 1, 
+              item: "Pizza", 
+              amount: Math.floor((Math.random() * 10) + 1), 
+              category: "Food", 
+              location: "KFC", 
+              spendOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10), 
+              createdOn: new Date(2020, 4, Math.floor((Math.random() * 30) + 1), 10, 10, 10) 
+           }, 
+        ]; 
+        return mockExpenseEntries; 
+      }
+      
+      title: string=''; 
+    expenseEntries:any[] = []; 
     timeChange = new Subject();
     ngOnInit() {
         setInterval(() => {
             this.timeChange.next(new Date().toString());
         }, 1000)
+
+        this.title = "Expense Entry List"; 
+        this.expenseEntries = this.getExpenseEntries(); 
     }
 }
