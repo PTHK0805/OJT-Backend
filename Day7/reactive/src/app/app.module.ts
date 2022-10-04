fd88a6483fe17f4cf369c440728bc1c6e9d1cff9
PTHK0805 <ptkyaw505@gmail.com>
2022-10-04 09:46:50 +0630

    Create Components.

--- Day7/reactive/src/app/app.module.ts
+++ Day7/reactive/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './components/test/test.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
