605a49465a33dba5f73d97995194cc30e68bc570
PTHK0805 <ptkyaw505@gmail.com>
2022-10-04 14:52:51 +0630

    Pipe Operator.

--- Day7/reactive/src/app/components/test/test.component.ts
+++ Day7/reactive/src/app/components/test/test.component.ts
@@ -14,6 +14,8 @@ export class TestComponent implements OnInit {
   val2: number = 0;
   mappedNumbers: any[] = [];
   val3: number = 0;
+  processedNumber: any[] = [];
+  val4: number = 0;
   constructor() { }
 
   ngOnInit(): void {
@@ -44,18 +46,30 @@ export class TestComponent implements OnInit {
     //console.log('Filterednumbers', this.filteredNumbers);
     //console.log('Total', this.val2);
 
-    const filteredNumbers$ = filter((num: number) => num > 5)(numbers$);
+    //const filteredNumbers$ = filter((num: number) => num > 5)(numbers$);
+    const filterFn = filter((num: number) => num > 5);
+    const filteredNumbers$ = filterFn(numbers$)
     filteredNumbers$.subscribe((num: number) => {
       this.filteredNumbers.push(num); this.val2 += num;
     })
     console.log('FilteredNumber : ', this.filteredNumbers);
     console.log('Total :', this.val2);
     
-    const mappedNumbers$ = map((num: number) => num + num)(numbers$);
+    //const mappedNumbers$ = map((num: number) => num + num)(numbers$);
+    const mapFn = map((num: number) => num + num);
+    const mappedNumbers$ = mapFn(numbers$);
     mappedNumbers$.subscribe((num: number) => { this.mappedNumbers.push(num); this.val3 += num });
     console.log('MappedNumbers', this.mappedNumbers);
     console.log('Total', this.val3);
     
+    const processedNumbers$ = numbers$.pipe(filterFn, mapFn);
+    processedNumbers$.subscribe({
+      next: (num: number) => { this.processedNumber.push(num); this.val4 += num },
+      error: (err: any) => console.log('Error: ', err),
+      complete: () => console.log('Observation completed...')
+    })
+    console.log("ProcessedNumber: ", this.processedNumber);
+    console.log("total: ", this.val4);
   }
 
 }
