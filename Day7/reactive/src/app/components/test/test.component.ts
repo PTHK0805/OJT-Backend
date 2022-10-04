b2015d1f16c5e1dd7abd7af51ae14b11d4099889
PTHK0805 <ptkyaw505@gmail.com>
2022-10-04 10:23:56 +0630

    filter operator.

--- Day7/reactive/src/app/components/test/test.component.ts
+++ Day7/reactive/src/app/components/test/test.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { from, of } from 'rxjs';
+import { filter, from, of } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -10,14 +10,16 @@ import { ajax } from 'rxjs/ajax';
 export class TestComponent implements OnInit {
   numbers: any = [];
   val1: number = 0;
+  filteredNumbers: any = [];
+  val2: number = 0;
   constructor() { }
 
   ngOnInit(): void {
     //const numbers$ = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     //const numbers$ = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     //numbers$.subscribe(data => {
-      //  console.log(data);
-      //})
+    //  console.log(data);
+    //})
     const numbers$ = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     // observer 
     //const observer = {
@@ -26,13 +28,22 @@ export class TestComponent implements OnInit {
     //  complete: () => console.log("Observation completed", this.numbers, this.val1)
     //};
     //numbers$.subscribe(observer);
-    numbers$.subscribe(num => {
-      this.numbers.push(num);
-      this.val1 += num;
-      console.log('Observation completed', this.numbers, this.val1)
-    });
+    //numbers$.subscribe(num => {
+    //  this.numbers.push(num);
+    //  this.val1 += num;
+    //  console.log('Observation completed', this.numbers, this.val1)
+    //});
 
-  
+    const filterFn = filter((num: number) => num > 5);
+    const filteredNumbers$ = filterFn(numbers$);
+    filteredNumbers$.subscribe((num: number) => {
+      this.filteredNumbers.push(num); this.val2 += num
+    });
+    console.log('Filterednumbers', this.filteredNumbers);
+    console.log('Total', this.val2);
+
+
+
   }
 
 }
