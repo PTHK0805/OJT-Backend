ea554893ff1c9643f9c6a808b1d123125552125f
PTHK0805 <ptkyaw505@gmail.com>
2022-10-05 14:11:34 +0630

    Change Application name to "Dependency Injection".

--- Day8/expense-manager/karma.conf.js
+++ Day8/expense-manager/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/expense-manager'),
+      dir: require('path').join(__dirname, './coverage/dependency-injection'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Day8/expense-manager/src/app/app.component.spec.ts
+++ Day8/expense-manager/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'expense-manager'`, () => {
+  it(`should have as title 'dependency-injection'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('expense-manager');
+    expect(app.title).toEqual('dependency-injection');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('expense-manager app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('dependency-injection app is running!');
   });
 });
