8ec933e240d1fa8bc5fbac788005814cc23db065
PTHK0805 <ptkyaw505@gmail.com>
2022-10-06 14:20:28 +0630

    Angular services demo.

--- Day9/angular-services/src/app/app.component.ts
+++ Day9/angular-services/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Product } from './interfaces/product';
+import { ProductService } from './services/product.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-services';
+    title = 'angular-services';
+    products: Product[] = [];
+    productService;
+
+    constructor() {
+        this.productService = new ProductService();
+    }
+
+    getProducts() {
+
+        this.products = this.productService.getProducts();
+    }
+
+    removeProducts() {
+        this.products = [];
+    }
 }
