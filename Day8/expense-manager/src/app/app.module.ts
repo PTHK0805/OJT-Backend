07111b801618e92e8d7b85b6eca6b6531fd2c2a8
PTHK0805 <ptkyaw505@gmail.com>
2022-10-07 11:57:34 +0630

    Angular Material.

--- Day8/expense-manager/src/app/app.module.ts
+++ Day8/expense-manager/src/app/app.module.ts
@@ -6,7 +6,12 @@ import { AppComponent } from './app.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { ChildComponent } from './components/child/child.component';
 import { MessagesComponent } from './components/messages/messages.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +21,14 @@ import { MessagesComponent } from './components/messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatAutocompleteModule,
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Day8/expense-manager/src/app/components/child/child.component.ts
+++ Day8/expense-manager/src/app/components/child/child.component.ts
@@ -2,7 +2,9 @@ import { HeroService } from 'src/app/services/hero.service';
 import { MessageService } from './../../services/message.service';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Hero } from 'src/app/interfaces/hero';
-
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
@@ -14,12 +16,27 @@ export class ChildComponent implements OnInit {
   @Input() heroes: Hero[] = [];
   @Output() btnClickCount = new EventEmitter();
   selectedHero?: Hero;
+
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions!: Observable<string[]>;
+
   constructor(private messageService: MessageService, private heroService: HeroService) { }
 
   ngOnInit(): void {
-    this.heroService.subject$.subscribe((data:any)=>{
+    this.heroService.subject$.subscribe((data: any) => {
       this.heroes = data;
-    })
+    });
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
   onSelect(hero: Hero): void {
