967c5dcaaf9fddc8fce086767faae069acb9ef6a
PTHK0805 <ptkyaw505@gmail.com>
2022-10-11 10:23:04 +0630

    Angular Material(Expansion Panel).

--- Day11/angular-materials/src/app/app.module.ts
+++ Day11/angular-materials/src/app/app.module.ts
@@ -7,20 +7,38 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DividerComponent } from './components/divider/divider.component';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { ExpansionPanelComponent } from './components/expansion-panel/expansion-panel.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DividerComponent
+    DividerComponent,
+    ExpansionPanelComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatDividerModule,
-    MatListModule
+    MatListModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    MatDatepickerModule,  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
