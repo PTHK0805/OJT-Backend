1cb65c2cac8300ccd2e0448d26a3b98690acb764
PTHK0805 <ptkyaw505@gmail.com>
2022-10-11 11:39:09 +0630

    Angular Material(Input).

--- Day11/angular-materials/src/app/app.module.ts
+++ Day11/angular-materials/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GridListComponent } from './components/grid-list/grid-list.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { IconComponent } from './components/icon/icon.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { IconComponent } from './components/icon/icon.component';
     ExpansionPanelComponent,
     FormFieldComponent,
     GridListComponent,
-    IconComponent
+    IconComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
