044e8fd1611607d5496191442a334d7d958829ab
Phyo Thiha Kyaw <67636515+PTHK0805@users.noreply.github.com>
2022-10-14 21:21:34 +0630

    Template Driven Form (Update).

--- Day14/reactive-form/src/app/assignments/template-driven-form/template-driven-form.component.ts
+++ Day14/reactive-form/src/app/assignments/template-driven-form/template-driven-form.component.ts
@@ -8,20 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateDrivenFormComponent implements OnInit {
   hidePassword = true;
   hideConfirmPassword = true;
-
-//  getPassword(value: any) {
-//    this.password1 = value;
-//  }
-//
-//  getConfirmPassword(value:any){
-//    this.confirmPassword1 = value;
-//  }
-
-  isMatch(password:any, confirmPassword:any) {
-    return ((password as string) === (confirmPassword as string) ? true : false)
-  }
+  
   constructor() { }
-
+      
   ngOnInit(): void {
   }
 
