a55d0c88f3611f50232d1ff82babd5243dee393d
PTHK0805 <ptkyaw505@gmail.com>
2022-10-17 13:46:00 +0630

    Authentication and Authorization.

--- Day15/angular-authentication/src/app/app-routing.module.ts
+++ Day15/angular-authentication/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpenseEntryListComponent } from './components/expense-entry-list/expense-entry-list.component';
+import { ExpenseEntryComponent } from './components/expense-entry/expense-entry.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { ExpenseGuard } from './expense.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'expenses', component: ExpenseEntryListComponent, canActivate: [ExpenseGuard]},
+  { path: 'expenses/detail/:id', component: ExpenseEntryComponent, canActivate: [ExpenseGuard]},
+  { path: '', redirectTo: 'expenses', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Day15/angular-authentication/src/app/app.module.ts
+++ Day15/angular-authentication/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { ExpenseEntryListComponent } from './components/expense-entry-list/expense-entry-list.component';
+import { ExpenseEntryComponent } from './components/expense-entry/expense-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    ExpenseEntryListComponent,
+    ExpenseEntryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
