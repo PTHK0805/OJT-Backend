31076bdd278204e0cfb29bd92585124e61d19167
PTHK0805 <ptkyaw505@gmail.com>
2022-10-20 10:56:37 +0630

    Angular Material Sorting.

--- Day18/AngularSortAndFilter/src/app/app-materials.module.ts
+++ Day18/AngularSortAndFilter/src/app/app-materials.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
-
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [],
@@ -12,7 +12,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 
   exports: [
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatSortModule
   ]
 })
 export class AppMaterialsModule { }
--- Day18/AngularSortAndFilter/src/app/components/table/table.component.ts
+++ Day18/AngularSortAndFilter/src/app/components/table/table.component.ts
@@ -3,6 +3,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { Employee } from 'src/app/services/data';
 import { MatTableDataSource } from '@angular/material/table';
 import { DataService } from 'src/app/services/data.service';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-table',
@@ -20,9 +21,11 @@ export class TableComponent implements AfterViewInit {
   }
   
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild('empTbSort') empTbSort!: MatSort;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.empTbSort;
   }
 
 }
